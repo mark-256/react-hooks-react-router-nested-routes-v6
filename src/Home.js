006dd02ec254df01ecb8158d0a55afeb03a51c94
Mark <markjohnguto@gmail.com>
2025-10-19 00:12:42 +0300

    modified

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+// Home.js
+import { Outlet, useOutletContext } from "react-router-dom";
+import UserCard from "../components/UserCard";
+
+function Home() {
+  const users = useOutletContext();
+  const userList = users.map((user) => <UserCard key={user.id} user={user} />);
+
+  return (
+    <main>
+      <h1>Home!</h1>
+      <Outlet context={users} />
+      {userList}
+    </main>
+  );
+}
+
+export default Home;
